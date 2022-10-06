0ecac34ffe00e00f4310a5aa6374536aad22e417
Yura Balint <46222135+supalapka@users.noreply.github.com>
2022-10-06 22:29:41 +0300

    authentication set up

---
+++ Clothes_Store/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Clothes_Store.Data;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(Clothes_Store.Areas.Identity.IdentityHostingStartup))]
+namespace Clothes_Store.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
