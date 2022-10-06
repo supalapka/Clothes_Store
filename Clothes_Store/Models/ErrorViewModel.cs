fab66618361747d62e70cee7f201242d5b7832bf
Yura Balint <46222135+supalapka@users.noreply.github.com>
2022-10-06 22:10:25 +0300

    Add project files.

---
+++ Clothes_Store/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Clothes_Store.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
