d43949a0664865b97eae7264cb74a65a73819108
Yura Balint <46222135+supalapka@users.noreply.github.com>
2022-10-09 14:30:44 +0300

    create cart controller and views for frontend

---
+++ Clothes_Store/Controllers/CartController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Clothes_Store.Controllers
+{
+    public class CartController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
