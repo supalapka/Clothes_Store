1c8ad6e4c63a12e152c7ca1e2e086a098d638634
Yura Balint <46222135+supalapka@users.noreply.github.com>
2022-10-10 14:49:16 +0300

    demo admin panel layout

---
+++ Clothes_Store/Controllers/AdminController.cs
@@ -0,0 +1,38 @@
+﻿using DbAccessLibrary.DbAccess;
+using DbAccessLibrary.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace Clothes_Store.Controllers
+{
+
+    [Authorize(Roles = "Administrator")]
+    public class AdminController : Controller
+    {
+        private readonly ClothesStoreDbContext _context;
+
+        public AdminController(ClothesStoreDbContext context)
+        {
+            _context = context;
+        }
+
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+
+        [Route("Admin/Add")]
+        public IActionResult AddClothes() { return View(); }
+
+        [Route("Admin/Add")]
+        [HttpPost]
+        public async Task<IActionResult> AddClothes(Clothes clothes)
+        {
+
+            return View();
+        }
+    }
+}
