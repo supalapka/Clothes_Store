98256dde7fc84d819a18900a746f9d5119f9ee55
Yura Balint <46222135+supalapka@users.noreply.github.com>
2022-10-15 19:32:51 +0300

    added js and images

--- Clothes_Store/wwwroot/js/site.js
+++ Clothes_Store/wwwroot/js/site.js
@@ -2,6 +2,52 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+
+const toggle = document.querySelector(".toggle");
+const menu = document.querySelector(".menu");
+const items = document.querySelectorAll(".item");
+
+//toggle mobile menu
+function toggleMenu() {
+    if (menu.classList.contains("active")) {
+        menu.classList.remove("active");
+        toggle.querySelector("a").innerHTML = "<i class='fas fa-bars'></i>";
+    } else {
+        menu.classList.add("active");
+        toggle.querySelector("a").innerHTML = "<i class='fas fa-times'></i>";
+    }
+}
+
+//Activate Submenu
+function toggleItem() {
+    if (this.classList.contains("submenu-active")) {
+        this.classList.remove("submenu-active");
+    } else if (menu.querySelector(".submenu-active")) {
+        menu.querySelector(".submenu-active").classList.remove("submenu-active");
+        this.classList.add("submenu-active");
+    } else {
+        this.classList.add("submenu-active");
+    }
+}
+
+//event listeners
+toggle.addEventListener("click", toggleMenu, false);
+for (let item of items) {
+    if (item.querySelector(".submenu")) {
+        item.addEventListener("click", toggleItem, false);
+    }
+    item.addEventListener("keypress", toggleItem, false);
+}
+//// JUST A SLIDER
+
+// MULTY ITEM SLIDER STARTS HERE
+
+console.log("================================");
+
+
+
+
 let slideIndex = 1;
 showSlides(slideIndex);
 
@@ -56,45 +102,3 @@ $(document).ready(function () {
         ],
     });
 });
-
-const toggle = document.querySelector(".toggle");
-const menu = document.querySelector(".menu");
-const items = document.querySelectorAll(".item");
-
-//toggle mobile menu
-function toggleMenu() {
-    if (menu.classList.contains("active")) {
-        menu.classList.remove("active");
-        toggle.querySelector("a").innerHTML = "<i class='fas fa-bars'></i>";
-    } else {
-        menu.classList.add("active");
-        toggle.querySelector("a").innerHTML = "<i class='fas fa-times'></i>";
-    }
-}
-
-//Activate Submenu
-function toggleItem() {
-    if (this.classList.contains("submenu-active")) {
-        this.classList.remove("submenu-active");
-    } else if (menu.querySelector(".submenu-active")) {
-        menu.querySelector(".submenu-active").classList.remove("submenu-active");
-        this.classList.add("submenu-active");
-    } else {
-        this.classList.add("submenu-active");
-    }
-}
-
-//event listeners
-toggle.addEventListener("click", toggleMenu, false);
-for (let item of items) {
-    if (item.querySelector(".submenu")) {
-        item.addEventListener("click", toggleItem, false);
-    }
-    item.addEventListener("keypress", toggleItem, false);
-}
-//// JUST A SLIDER
-
-// MULTY ITEM SLIDER STARTS HERE
-
-console.log("================================");
-
