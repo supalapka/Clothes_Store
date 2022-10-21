3b585d8b37e2342d9ebfc41b0f155fa71873265a
Vladislav <vladlipchey05@gmail.com>
2022-10-21 18:59:53 +0300

    feat: add script for choosen img display and some styling

--- Clothes_Store/wwwroot/js/site.js
+++ Clothes_Store/wwwroot/js/site.js
@@ -102,3 +102,16 @@ $(document).ready(function () {
         ],
     });
 });
+//ADMIN PANEL ADD CLOTHES
+//THIS SCRIPT ADDS CHOOSEN IMAGE OF USER? DISPLAYED ON RIGHT DIV
+//CLOSES 63E9
+let wrapper = document.querySelector('.img_main');
+
+function donwload(input) {
+    let file = input.files[0];
+    let reader = new FileReader();
+    reader.readAsDataURL(file);
+    reader.onload = function () {
+        img1.src = reader.result;
+    }
+}
