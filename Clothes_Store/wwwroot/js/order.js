f0d76f76ff9de61a1bebbf540712ef39f671e500
Vladislav <vladlipchey05@gmail.com>
2022-10-25 22:51:21 +0300

    feat:add to order page. fix: remove one-named-tags from css file

---
+++ Clothes_Store/wwwroot/js/order.js
@@ -0,0 +1,25 @@
+﻿price = document.querySelector('.price_');
+btnPlus = document.querySelector('.plus');
+btnMinus = document.querySelector('.minus');
+count = document.querySelector('.count');
+counter = 1;
+currentPrice = parseInt(price.textContent);
+const data = parseInt(price.textContent);
+btnPlus.addEventListener('click', () => {
+    currentPrice += data;
+    counter += 1;
+    price.textContent = `${currentPrice} грн`;
+    count.textContent = counter;
+});
+btnMinus.addEventListener('click', () => {
+    if (counter == 1) {
+        price.textContent = `${currentPrice} грн`;
+        count.textContent = counter;
+    }
+    else {
+        currentPrice -= data;
+        counter -= 1;
+    }
+    price.textContent = `${currentPrice} грн`;
+    count.textContent = counter;
+});
