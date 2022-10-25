8916e8c83b906e2eff52daff1f76eb892ed8c8cb
Yura Balint <46222135+supalapka@users.noreply.github.com>
2022-10-25 21:14:44 +0300

    created clothes controller

---
+++ Clothes_Store/Controllers/ClothesController.cs
@@ -0,0 +1,28 @@
+﻿using DbAccessLibrary.DbAccess;
+using DbAccessLibrary.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+
+namespace Clothes_Store.Controllers
+{
+    public class ClothesController :Controller
+    {
+        private readonly ClothesStoreDbContext _context;
+
+        public ClothesController(ClothesStoreDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult ClothesDetails(int id)
+        {
+            return View();
+        }
+
+    }
+}
