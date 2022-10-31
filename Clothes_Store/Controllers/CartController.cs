108b997d98853880a3c2645686cfa805f8468b6d
Yura Balint <46222135+supalapka@users.noreply.github.com>
2022-10-31 22:44:52 +0200

    cart + and -  buttons interact with api

---
+++ Clothes_Store/Controllers/CartController.cs
@@ -0,0 +1,26 @@
+﻿using DbAccessLibrary.DbAccess;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace Clothes_Store.Controllers
+{
+    public class CartController : Controller
+    {
+        private readonly ClothesStoreDbContext _context;
+
+        public CartController(ClothesStoreDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task IncrementItemQuantity(int cartId)
+        {
+            await CartRepository.IncrementQuantity(cartId,_context);
+        }
+
+        public async Task DecrementItemQuantity(int cartId)
+        {
+            await CartRepository.DecrementQuantity(cartId, _context);
+        }
+    }
+}
