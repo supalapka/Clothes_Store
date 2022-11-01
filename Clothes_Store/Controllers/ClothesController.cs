17709584fc0dd0ff4605119cc4b8c545d2503dee
Yura Balint <46222135+supalapka@users.noreply.github.com>
2022-11-01 13:14:56 +0200

    clothing ad creation removed from admin panel and added to clothing controller

--- Clothes_Store/Controllers/ClothesController.cs
+++ Clothes_Store/Controllers/ClothesController.cs
@@ -1,7 +1,10 @@
-﻿using DbAccessLibrary.DbAccess;
+﻿using ClassLibrary;
+using Clothes_Store.Models;
+using DbAccessLibrary.DbAccess;
 using DbAccessLibrary.Models;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using System.Linq;
 using System.Security.Claims;
 using System.Threading.Tasks;
 
@@ -22,7 +25,7 @@ namespace Clothes_Store.Controllers
             return View();
         }
 
-        public IActionResult ClothesDetails(int id)
+        public IActionResult Details(int id)
         {
             var clothes = ClothesRepository.GetById(id,_context);
             return View(clothes);
@@ -32,13 +35,37 @@ namespace Clothes_Store.Controllers
         {
             var userId = User.FindFirstValue(ClaimTypes.NameIdentifier);
             await CartRepository.CreateAsync(id, quantity, size, userId, _context);
-            return RedirectToAction("ClothesDetails", new { id });
+            return RedirectToAction("Details", new { id });
         }
 
-
-        public IActionResult ClothesList()
+        public IActionResult List()
         {
             return View();
         }
+
+
+        [Route("Clothes/ad")]
+        public IActionResult CreateAd() { return View(); }
+
+
+        [Route("Clothes/ad")]
+        [HttpPost]
+        public async Task<IActionResult> CreateAd(ClothesPreview clothes)
+        {
+            var fileBytesImage = FunctionsLib.FileToByteArrayAsync(clothes.PreviwImageFileInput); //convert clothes img to byte array for db storing
+            string sellerId = User.FindFirstValue(ClaimTypes.NameIdentifier);
+            int id = await ClothesRepository.CreateAndReturnIdAsync(name: clothes.Name,
+                previewImage: fileBytesImage,
+                clothingCategory: clothes.Category,
+                gender: clothes.Gender,
+                color: clothes.Color,
+                sellerId: sellerId,
+                price: clothes.Price,
+                typesOfClothes: clothes.TypeOfClothes,
+                _context: _context);
+
+            return RedirectToAction("Details", new { id });
+        }
+
     }
 }
