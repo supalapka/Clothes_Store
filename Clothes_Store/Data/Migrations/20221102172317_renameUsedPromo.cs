afb05e7ddec7b33581029f37829b3916617eb864
Yura Balint <46222135+supalapka@users.noreply.github.com>
2022-11-02 22:23:24 +0200

    renamed usedPromo

---
+++ Clothes_Store/Data/Migrations/20221102172317_renameUsedPromo.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Clothes_Store.Data.Migrations
+{
+    public partial class renameUsedPromo : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameTable(
+             name: "UsedPromocode",
+             newName: "UsedPromocodes"
+         );
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameTable(
+             name: "UsedPromocodes",
+             newName: "UsedPromocode"
+             );
+        }
+    }
+}
