44b903d842480a1e50d7e4250aeb2eee22766fea
Yura Balint <46222135+supalapka@users.noreply.github.com>
2022-11-09 13:03:32 +0200

    remove unimplemented props

--- DbAccessLibrary/Models/Clothes.cs
+++ DbAccessLibrary/Models/Clothes.cs
@@ -9,7 +9,6 @@ namespace DbAccessLibrary.Models
     public class Clothes
     {
         public int Id { get; set; }
-      
         public string Name { get; set; }
         public int Price { get; set; }
         public TypesOfClothes TypeOfClothes { get; set; }
@@ -17,8 +16,6 @@ namespace DbAccessLibrary.Models
         public Gender Gender { get; set; }
         public Colors Color { get; set; }
         public byte[] PreviewImage { get; set; }
-        public int Rating { get; set; }
-        public int CountSell { get; set; }
         public string SellerId { get; set; }
         public List<Cart> Carts { get; set; }
     }
