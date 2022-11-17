590ff94e214e813d0ddb87a0b97ef26646f5ab10
Vladislav <vladlipchey05@gmail.com>
2022-11-17 23:53:07 +0200

    remove unused js code

--- Clothes_Store/wwwroot/js/site.js
+++ Clothes_Store/wwwroot/js/site.js
@@ -19,26 +19,10 @@ function toggleMenu() {
     }
 }
 
-//Activate Submenu
-function toggleItem() {
-    if (this.classList.contains("submenu-active")) {
-        this.classList.remove("submenu-active");
-    } else if (menu.querySelector(".submenu-active")) {
-        menu.querySelector(".submenu-active").classList.remove("submenu-active");
-        this.classList.add("submenu-active");
-    } else {
-        this.classList.add("submenu-active");
-    }
-}
 
 //event listeners
 toggle.addEventListener("click", toggleMenu, false);
-for (let item of items) {
-    if (item.querySelector(".submenu")) {
-        item.addEventListener("click", toggleItem, false);
-    }
-    item.addEventListener("keypress", toggleItem, false);
-}
+
 $(document).ready(function () {
     $(".slider").slick({
         arrows: false,
